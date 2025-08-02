d31285d22370f8f9abf5ffb5d07535f5d053155b
ZhannaOvcharenko <ZOvcharenko99@gmail.com>
2025-08-02 23:09:03 +0300

    change ebay_page

--- pages/ebay_page.py
+++ pages/ebay_page.py
@@ -4,7 +4,7 @@ from selene import browser, have
 class EbayPage:
 
     def open(self):
-        browser.open('https://www.ebay.com')
+        browser.open('https://www.ebay.com/?_nkw=')
         return self
 
     def search(self, query: str):
