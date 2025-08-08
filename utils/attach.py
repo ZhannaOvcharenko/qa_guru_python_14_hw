1072501076da01f67ade14ae3d0f8101b8bb00f5
ZhannaOvcharenko <ZOvcharenko99@gmail.com>
2025-08-08 15:14:24 +0300

    change attach

--- utils/attach.py
+++ utils/attach.py
@@ -1,47 +1,25 @@
 import allure
-import requests
-from selene import browser
-import os
+from allure_commons.types import AttachmentType
 
 
-def add_screenshot():
-    allure.attach(
-        browser.driver.get_screenshot_as_png(),
-        name="Screenshot",
-        attachment_type=allure.attachment_type.PNG
-    )
+def add_screenshot(browser):
+    png = browser.driver.get_screenshot_as_png()
+    allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
 
 
-def add_html():
-    allure.attach(
-        browser.driver.page_source,
-        name="Page Source",
-        attachment_type=allure.attachment_type.HTML
-    )
+def add_logs(browser):
+    log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
+    allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
 
 
-def add_logs():
-    try:
-        logs = browser.driver.get_log("browser")
-        log_text = "\n".join([f"{log['level']}: {log['message']}" for log in logs])
-        allure.attach(log_text, name="Browser Logs", attachment_type=allure.attachment_type.TEXT)
-    except:
-        pass
+def add_html(browser):
+    html = browser.driver.page_source
+    allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
 
 
-def add_video(session_id: str):
-    selenoid_url = os.getenv("SELENOID_URL")
-    if not selenoid_url or not session_id:
-        return
-
-    video_url = f"https://{selenoid_url}/video/{session_id}.mp4"
-    try:
-        response = requests.get(video_url, verify=False)
-        if response.status_code == 200:
-            allure.attach(
-                response.content,
-                name="Test Video",
-                attachment_type=allure.attachment_type.MP4
-            )
-    except Exception as e:
-        print(f"[attach.py] Не удалось прикрепить видео: {e}")
+def add_video(browser):
+    video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
+    html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
+           + video_url \
+           + "' type='video/mp4'></video></body></html>"
+    allure.attach(html, 'video_' + browser.driver.session_id, AttachmentType.HTML, '.html')
