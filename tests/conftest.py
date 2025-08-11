bc8bf3ff71d2e177a1a7eaac2d314e739924d34c
ZhannaOvcharenko <ZOvcharenko99@gmail.com>
2025-08-11 11:49:10 +0300

    change conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,44 +1,36 @@
-import os
-import allure
 import pytest
-from dotenv import load_dotenv
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene.support.shared import browser
 from utils import attach
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-@pytest.fixture(scope="session", autouse=True)
-def load_env():
-    load_dotenv()
-
-
-selenoid_login = os.getenv("SELENOID_LOGIN")
-selenoid_pass = os.getenv("SELENOID_PASS")
-selenoid_url = os.getenv("SELENOID_URL")
-
-
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope='function', autouse=True)
 def setup_browser():
-    options = Options()
-    options.add_argument("--incognito")
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
 
-    capabilities = {
+    if not all([selenoid_login, selenoid_pass, selenoid_url]):
+        raise ValueError(
+            "Ошибка: переменные SELENOID_LOGIN, SELENOID_PASS или SELENOID_URL не заданы. "
+            "Убедитесь, что .env существует или переменные переданы в окружение (например, через Jenkins)."
+        )
+
+    options = Options()
+    selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": "128.0",
         "selenoid:options": {
             "enableVNC": True,
-            "enableVideo": True,
-            "enableLog": True
-        },
-        "goog:loggingPrefs": {
-            "browser": "ALL",
-            "driver": "ALL"
+            "enableVideo": True
         }
     }
-    options.capabilities.update(capabilities)
-    options.set_capability("selenoid:options", capabilities["selenoid:options"])
-    options.set_capability("goog:loggingPrefs", capabilities["goog:loggingPrefs"])
+    options.capabilities.update(selenoid_capabilities)
 
     driver = webdriver.Remote(
         command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
@@ -46,13 +38,9 @@ def setup_browser():
     )
 
     browser.config.driver = driver
-    browser.config.timeout = 20
     browser.config.base_url = "https://www.ebay.com"
     browser.driver.maximize_window()
-    browser.driver.delete_all_cookies()
-
-    with allure.step("Open eBay main page"):
-        browser.open('/')
+    browser.config.timeout = 10
 
     yield
 
